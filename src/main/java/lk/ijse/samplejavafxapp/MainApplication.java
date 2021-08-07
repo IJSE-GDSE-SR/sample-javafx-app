bc289c2859babc808aabf7644d20998939b831ab
Sandul Renuja <sandulrenuja@gmail.com>
2021-08-07 12:30:20 +0530

    Setup files and directory structure

--- src/main/java/lk/ijse/samplejavafxapp/MainApplication.java
+++ src/main/java/lk/ijse/samplejavafxapp/MainApplication.java
@@ -1,20 +1,28 @@
+/**
+ * @author Sandul Renuja <sandulrenuja@gmail.com>
+ * @since  7/8/21
+ */
+
 package lk.ijse.samplejavafxapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Sample JavaFX App");
-        stage.setScene(scene);
-        stage.show();
+//        Parent load = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Customers.fxml")));
+//        Scene scene = new Scene(load);
+//        stage.setScene(scene);
+//        stage.setTitle("Customer Management");
+//        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/**
+ * @author Sandul Renuja <sandulrenuja@gmail.com>
+ * @since  7/8/21
+ */
+
 module lk.ijse.samplejavafxapp {
     requires javafx.controls;
     requires javafx.fxml;
@@ -5,4 +10,6 @@ module lk.ijse.samplejavafxapp {
 
     opens lk.ijse.samplejavafxapp to javafx.fxml;
     exports lk.ijse.samplejavafxapp;
+    exports lk.ijse.samplejavafxapp.controller;
+    opens lk.ijse.samplejavafxapp.controller to javafx.fxml;
 }
